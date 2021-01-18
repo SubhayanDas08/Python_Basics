acda7f1a9b0b10ea68e68b46180077d8e206343c
SubhayanDas08 <nips.subhayan@gmail.com>
2021-01-18 15:18:50 +0530

    18th Jan 2021

---
+++ Basics/Strings/escape_characters.py
@@ -0,0 +1,18 @@
+# Escape Character
+# To insert characters that are illegal in a string, use an escape character.
+
+# An escape character is a backslash \ followed by the character you want to insert.
+
+# \'	Single Quote	
+# \\	Backslash	
+# \n	New Line	
+# \r	Carriage Return	
+# \t	Tab	
+# \b	Backspace	
+# \f	Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value
+
+txt = "We are the so-called \"Vikings\" from the north."
+
+print(txt)
