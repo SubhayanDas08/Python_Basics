40e0e625cc933f86cf3f06cea92045419b16a71f
SubhayanDas08 <nips.subhayan@gmail.com>
2021-01-18 11:14:06 +0530

    18th Jan 2021

---
+++ Basics/strings.py
@@ -0,0 +1,47 @@
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+# Strings are Arrays
+
+a = "Hello, World!"
+print(a[1])
+
+# Looping Through a String
+
+for x in "banana":
+  print(x)
+
+# String Length
+
+a = "Hello, World!"
+print(len(a))
+
+# Check String
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+# Check if NOT
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("Yes, 'expensive' is NOT present.")
